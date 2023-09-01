cb77fb091a524e96d2a2c9f27e779b5e149e0a8b
Talal H. Syed <th.innovate@outlook.com>
2023-09-01 10:56:57 +0500

    Update api.py

--- maa/api.py
+++ maa/api.py
@@ -3,8 +3,8 @@ import frappe
 @frappe.whitelist()
 def sort_delivery_order(docname):
     cur = frappe.get_doc("Delivery Trip", docname)
-    frappe.msgprint(str(cur))
+    frappe.msgprint(str(cur.table))
 
-    for i, item in enumerate(sorted(cur.table._range, key=lambda item: item.distance), start=1):
+    for i, item in enumerate(sorted(cur.table, key=lambda item: item.distance), start=1):
         item.idx = i
         frappe.msgprint(i)

f71c8c9cb90bc4f45d9e0455524e329568830e32
Hidayat ali <136191740+Inshasis@users.noreply.github.com>
2023-09-04 13:18:48 +0530

    change Debit to Credit

--- maa/maa/doctype/maa_coins_transaction/maa_coins_transaction.py
+++ maa/maa/doctype/maa_coins_transaction/maa_coins_transaction.py
@@ -8,14 +8,14 @@ class MAACoinsTransaction(Document):
 	# pass
 
 	def validate(self):
-		if self.transaction_type == 'Debit':
+		if self.transaction_type == 'Credit':
 			coin_wallet = frappe.get_value('MAA Coins Wallet',{'name':self.coins_wallet},'coins')
 			sum_coin = int(coin_wallet) + int(self.transaction_coins) 
 			# Update Coins in MAA Coins coin_wallet
 			frappe.db.set_value('MAA Coins Wallet', self.coins_wallet, {
 				'coins': str(sum_coin)
 			})
-			frappe.msgprint("Coins Debit Sucessfull {0}".format(self.coins_wallet))
+			frappe.msgprint("Coins Credit Sucessfull {0}".format(self.coins_wallet))
 		else:
 			coin_wallet = frappe.get_value('MAA Coins Wallet',{'name':self.coins_wallet},'coins')
 			sum_coin = int(coin_wallet) - int(self.transaction_coins) 

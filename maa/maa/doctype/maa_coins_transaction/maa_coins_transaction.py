d161936a3132494be04d9885c280fe7682bc9f63
Hidayat ali <136191740+Inshasis@users.noreply.github.com>
2023-09-06 20:13:21 +0530

    Update maa_coins_transaction.py

--- maa/maa/doctype/maa_coins_transaction/maa_coins_transaction.py
+++ maa/maa/doctype/maa_coins_transaction/maa_coins_transaction.py
@@ -5,32 +5,32 @@ import frappe
 from frappe.model.document import Document
 
 class MAACoinsTransaction(Document):
-	pass
+	# pass
 
-	# def after_insert(self):
-	# 	if self.transaction_type == 'Credit':
-	# 		coin_wallet = frappe.db.get_value('MAA Coins Wallet',{'name':self.coins_wallet},'coins')
+	def after_insert(self):
+		if self.transaction_type == 'Credit':
+			coin_wallet = frappe.db.get_value('MAA Coins Wallet',{'name':self.coins_wallet},'coins')
 			
-	# 		sum_coin = int(coin_wallet) + int(self.transaction_coins) 
-	# 		# Update Coins in MAA Coins coin_wallet
-	# 		frappe.db.set_value('MAA Coins Wallet', self.coins_wallet, {
-	# 			'coins': sum_coin
-	# 		})
-	# 		# Update Coins in Customer
-	# 		frappe.db.set_value('Customer', self.customer, {
-	# 			'total_coins': sum_coin
-	# 		})
-	# 		# frappe.msgprint("Coins Credit Sucessfull {0}".format(self.coins_wallet))
-	# 	else:
-	# 		coin_wallet = frappe.db.get_value('MAA Coins Wallet',{'name':self.coins_wallet},'coins')
-	# 		sum_coin = int(coin_wallet) - int(self.transaction_coins) 
-	# 		# Update Coins in MAA Coins coin_wallet
-	# 		frappe.db.set_value('MAA Coins Wallet', self.coins_wallet, {
-	# 			'coins': sum_coin
-	# 		})
+			sum_coin = int(coin_wallet) + int(self.transaction_coins) 
+			# Update Coins in MAA Coins coin_wallet
+			frappe.db.set_value('MAA Coins Wallet', self.coins_wallet, {
+				'coins': sum_coin
+			})
+			# Update Coins in Customer
+			frappe.db.set_value('Customer', self.customer, {
+				'total_coins': sum_coin
+			})
+			# frappe.msgprint("Coins Credit Sucessfull {0}".format(self.coins_wallet))
+		else:
+			coin_wallet = frappe.db.get_value('MAA Coins Wallet',{'name':self.coins_wallet},'coins')
+			sum_coin = int(coin_wallet) - int(self.transaction_coins) 
+			# Update Coins in MAA Coins coin_wallet
+			frappe.db.set_value('MAA Coins Wallet', self.coins_wallet, {
+				'coins': sum_coin
+			})
 
-	# 		# Update Coins in Customer
-	# 		frappe.db.set_value('Customer', self.customer, {
-	# 			'total_coins': sum_coin
-	# 		})
-	# 		# frappe.msgprint("Coins Debit Sucessfull {0}".format(self.coins_wallet))
+			# Update Coins in Customer
+			frappe.db.set_value('Customer', self.customer, {
+				'total_coins': sum_coin
+			})
+			# frappe.msgprint("Coins Debit Sucessfull {0}".format(self.coins_wallet))
